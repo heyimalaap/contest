0935c458960c70d68ad46e5e4d52217aaefcbcff
Alaap Surendran <alaapsurendran@gmail.com>
2020-07-31 21:23:53 +0530

    Clean stuff! also added templates

---
+++ template/c++17.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define PI  3.141592
+#define MOD 1000000007
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+
+    return 0;
+}
+
